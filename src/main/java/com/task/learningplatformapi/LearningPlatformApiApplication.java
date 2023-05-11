3f8f5ce16e3b4616d5465da777fb2c0e9e38cb79
Alex Contreras <alexcontreras@Alexs-MacBook-Pro.local>
2023-05-11 15:15:42 -0500

    Initial commit

---
+++ src/main/java/com/task/learningplatformapi/LearningPlatformApiApplication.java
@@ -0,0 +1,13 @@
+package com.task.learningplatformapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningPlatformApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningPlatformApiApplication.class, args);
+	}
+
+}
